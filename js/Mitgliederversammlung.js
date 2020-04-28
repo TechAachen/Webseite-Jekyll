2b4216b6323ccb33e591f0632159e3da27b9523f
Raphael Lehmann <raphael@rleh.de>
2020-04-28 17:46:32 +0200

    Add iCalendar ICS file with meeting events

--- js/Mitgliederversammlung.js
+++ js/Mitgliederversammlung.js
@@ -2,31 +2,30 @@
 ---
 
 function futureDate(a) {
-	var yesterday = new Date();
-	yesterday.setDate(yesterday.getDate() - 1);
-	return a[0] > yesterday;
+    var yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    return a[0] > yesterday;
 }
 
 function sortDates(a,b) {
-	return a[0] - b[0];
+    return a[0] - b[0];
 }
 
 function Mitgliederversammlung() {
-	var termine = [
-	{% for termin in site.data.mitgliederversammlung_kalender -%}
-	[new Date('{{ termin.datum }}'), '{{ termin.uhrzeit }}', '{{ termin.ort }}'],
-	{% endfor -%}
-	];
+    var termine = [
+    {% for termin in site.data.calendar -%}
+    [new Date('{{ termin.datum }}'), '{{ termin.uhrzeit }}', '{{ termin.ort }}', '{{ termin.title }}'],
+    {% endfor -%}
+    ];
 
-	var next_meeting = termine.filter(futureDate).sort(sortDates)[0];
+    var next_meeting = termine.filter(futureDate).sort(sortDates)[0];
 
     document.getElementById("mitgliederversammlung_text").innerHTML =
-    	"Die nächste <i>Geschäftsführende Mitgliederversammlung</i> findet am " +
-    	next_meeting[0].toLocaleDateString("de-DE", {day: "numeric", month: 'long', year: "numeric"}) +
-    	" um " + next_meeting[1] +
-    	" statt. </br>" +
-    	"Versammlungsort: " + next_meeting[2];
-
+        "Die nächste <i>" + next_meeting[3] + "</i> findet am " +
+        next_meeting[0].toLocaleDateString("de-DE", {day: "numeric", month: 'long', year: "numeric"}) +
+        " um " + next_meeting[1] +
+        " statt. " +
+        "Versammlungsort: " + next_meeting[2];
 }
 
 Mitgliederversammlung();
